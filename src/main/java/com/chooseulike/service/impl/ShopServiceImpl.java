e2bb4ec44196e1ffb0c0aa3e9b583484297b481b
buptwqz <879680229@qq.com>
2025-01-07 22:45:33 +0800

    缓存空对象

--- src/main/java/com/chooseulike/service/impl/ShopServiceImpl.java
+++ src/main/java/com/chooseulike/service/impl/ShopServiceImpl.java
@@ -15,8 +15,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.chooseulike.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.chooseulike.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.chooseulike.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -40,8 +39,12 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.ok(shop);
         }
 
+        if(shopJson != null){
+            return Result.fail("店铺信息不存在");
+        }
         Shop shop = getById(id);
         if (shop == null) {
+            stringRedisTemplate.opsForValue().set(key, "",CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在！");
         }
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
